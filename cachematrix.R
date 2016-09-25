a8291687780fea6a7d63d6c99013317b02317a8d
Andrew Price <andrewdavidprice@gmail.com>
2016-09-25 12:10:31 -0400

    Updated ProgrammingAssigment2 with cash functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Inverse of a Matrix:
+## Cashes the iverse of a matrix instead of constatnly computing it.
 
-## Write a short comment describing this function
+## This function creates a matric object and cashes its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL 
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  Get <- function() x
+  serInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Take makeCashMatrix function ans computed the inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  in <- x$getInverse()
+  if (!is.null(inv)) {
+    message("Receieving Cashed Information")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
